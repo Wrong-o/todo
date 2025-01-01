bab2f1f006d4e07233404870d5788036437cfd83
Wrong-o <otto@rongedal.se>
2025-01-01 21:58:35 +0100

    Added submit form

--- main.js
+++ main.js
@@ -6,4 +6,15 @@ todoList.forEach(item => {
   let listItem = document.createElement("li");
   listItem.textContent = item;
   listElement.appendChild(listItem);
+
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("todo-checkbox");
+
+  let span = document.createElement("span");
+  span.textContent = item;
+
+  listItem.appendChild(checkbox);
+
+  listElement.appendChild(listItem);
 });
